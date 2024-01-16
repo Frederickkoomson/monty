8000d95561a9f881b2a666ee1d0130defd612467
frederickkoomson <frederickkoomson25@gmail.com>
2024-01-16 15:37:48 -0800

    some added files

---
+++ monty.h
@@ -0,0 +1,35 @@
+/* monty.h */
+
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#ifndef _SSIZE_T_DEFINED
+#define _SSIZE_T_DEFINED
+typedef long ssize_t;
+#endif
+
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+typedef struct instruction_s
+{
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Function prototypes */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+/* Add more function prototypes as needed */
+
+#endif /* MONTY_H */
+
---
+++ monty_interpreter.c
@@ -0,0 +1,66 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 1024
+
+int main(int argc, char *argv[])
+{
+    FILE *file;
+    stack_t *stack = NULL;
+    char *line = NULL;
+    
+
+    unsigned int line_number = 0;
+    char *opcode;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    file = fopen(argv[1], "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL)
+    {
+        line_number++;
+
+        
+        if (line[strlen(line) - 1] != '\n' && !feof(file))
+        {
+            fprintf(stderr, "Error: Line too long at line %u\n", line_number);
+            free(line);
+            fclose(file);
+            return EXIT_FAILURE;
+        }
+
+        opcode = strtok(line, " \t\n");
+
+        if (opcode != NULL)
+        {
+            if (strcmp(opcode, "push") == 0)
+            {
+                push(&stack, line_number);
+            }
+            else if (strcmp(opcode, "pall") == 0)
+            {
+                pall(&stack, line_number);
+            }
+            
+        }
+    }
+
+    free(line);
+    fclose(file);
+
+    
+
+    return EXIT_SUCCESS;
+}
+
