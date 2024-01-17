e94c107ffe1366ef42b232fa6ecca318208f4b38
frederickkoomson <frederickkoomson25@gmail.com>
2024-01-17 04:33:21 -0800

    some added

--- monty.h
+++ monty.h
@@ -1,37 +1,59 @@
-/* monty.h */
+#ifndef __MONTY_H__
+#define __MONTY_H__
 
-#ifndef MONTY_H
-#define MONTY_H
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#include <stdbool.h>
 #include <string.h>
 
+#define DELIMS "\n \r\t"
 
-#ifndef _SSIZE_T_DEFINED
-#define _SSIZE_T_DEFINED
-
-typedef long ssize_t;
-#endif
-
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* Function prototypes */
+extern stack_t **global_head;
+
+void global_free(void);
+
+void read_file(char *file, stack_t **stack);
+void parse_command(stack_t **stack, char *op, unsigned int line_num);
+
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
-/* Add more function prototypes as needed */
+void swap(stack_t **stack, unsigned int line_number);
 
-#endif /* MONTY_H */
+#endif
